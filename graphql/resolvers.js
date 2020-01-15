358ea7e4e358efb412dc9676a3bae45b5078e5e6
jaemin_seo <sjm@neochart.co.kr>
2020-01-15 10:04:04 +0900

    mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,13 @@
-const jaemin = {
-  name: "SeoJaemin",
-  age: 32,
-  gender: "male"
-};
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
+
 const resolvers = {
   Query: {
-    pserson: () => jaemin
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id)
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id)
   }
 };
 
