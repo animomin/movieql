77021befd334a836a5f496ee00a04b889b7bd0fd
jaemin_seo <sjm@neochart.co.kr>
2020-01-15 10:35:00 +0900

    GraphQL finish

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,19 @@
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: 2,
-    name: "The Godfather I",
-    score: 99
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2
-  }
-];
+import axios from "axios";
+const API_URL = "https://yts-proxy.now.sh/list_movies.json";
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios.get(API_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  });
+  return movies;
 };
-
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
+// fetch(`${API_URL}`)
+//   .then(res => res.json())
+//   .then(json => json.data.movies);
